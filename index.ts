2239a1d16e2c63839110f093aa60f417d5f2d8aa
Jin <jjjinyong@naver.com>
2022-03-02 01:31:17 -0500

    init

---
+++ index.ts
@@ -0,0 +1,12 @@
+import * as Discord from "discord.js";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + '/key.env'})
+const token = process.env.CLIENT_TOKEN;
+
+const intents = new Discord.Intents(3608);
+//  * @property {IntentsResolvable} intents Intents to enable for this connection
+const client = new Discord.Client({ intents });
+
+
+client.login(token); //login bot using token
